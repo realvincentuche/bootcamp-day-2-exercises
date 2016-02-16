20d378a7a27444fdbd77c3fb178798ce30f292b0
Vincent Uchenna <uchgames@gmail.com>
2016-02-16 14:53:39 +0100

    Fourth changes to my ruby file: String-loop using the while loop

--- String-loop.rb
+++ String-loop.rb
@@ -1,10 +1,15 @@
-puts "Enter String text"
-text = gets.chomp
+# String-loop.rb To print out the uppercase of each letter with a number in series each on a new line
 
-words = text.split("")
-a = 1
-words.each {
-    |e|
-    puts "#{a}. #{e.upcase}"
-	a += 1
-}
+puts "Enter String Text:" # prints out the 'puts' string
+
+text = gets.chomp # gets the string from the keyboard input
+
+i = 0 # set counter to zero
+
+while i<text.length # start while loop
+
+    puts "#{i+1} #{text[i].upcase}" # prints out each letter of string (upper case) on a new line
+	
+	i += 1 # increment counter
+	
+end # end while loop
